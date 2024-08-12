f925828bbc1a70b357a832ad011927f3c51a1518
Thiago Dutra Nascimento <thiagoduna@hotmail.com>
2024-08-12 14:52:03 -0300

    new

--- src/app/AppGrafos.java
+++ src/app/AppGrafos.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class AppGrafos {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String filename = "entrada.txt";
+
+        try (BufferedReader buffer = new BufferedReader(new FileReader(filename))){
+            String linha;
+            int quantidadeCidades = Integer.parseInt(buffer.readLine()); // quantidade de cidades na primeira linha do arquivo
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+
+
+
     }
 }
